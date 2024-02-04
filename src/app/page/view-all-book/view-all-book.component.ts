6f9fe0faead521820abbe92b1e2031da261abed3
janath <janathhma@gmail.com>
2024-02-04 17:13:00 +0530

    add book component

--- src/app/page/view-all-book/view-all-book.component.ts
+++ src/app/page/view-all-book/view-all-book.component.ts
@@ -1,12 +1,30 @@
-import { Component } from '@angular/core';
+import { HttpClient,HttpClientModule } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-view-all-book',
   standalone: true,
-  imports: [],
+  imports: [HttpClientModule,FormsModule,CommonModule],
   templateUrl: './view-all-book.component.html',
   styleUrl: './view-all-book.component.css'
 })
-export class ViewAllBookComponent {
+export class ViewAllBookComponent implements OnInit{
+  private http;
+  public bookList:any={};
 
+  constructor(private httpClient:HttpClient){
+    this.http=httpClient;
+  }
+
+  ngOnInit(): void {
+      this.LoadBook();
+  }
+
+  private LoadBook(){
+    this.http.get('http://localhost:8080/book/get').subscribe((data)=>{
+      this.bookList=data;
+    })
+  }  
 }
