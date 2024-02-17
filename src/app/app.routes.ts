f1dabe5844a6686c966e5cfa89ebdc9a4a62f7f0
janath <janathhma@gmail.com>
2024-02-17 07:37:54 +0530

    set routing for login and view all books components

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,14 @@
 import { Routes } from '@angular/router';
+import { LoginComponent } from './page/login/login.component';
+import { ViewAllBookComponent } from './page/view-all-book/view-all-book.component';
 
 export const routes: Routes = [
     {
-        path:'login',
-        component:
+        path:"login",
+        component: LoginComponent
+    },
+    {
+        path:"view-books",
+        component:ViewAllBookComponent
     }
 ];
