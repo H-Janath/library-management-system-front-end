2f394b08e1b50588dddc2a507d70580cace75069
janath <janathhma@gmail.com>
2024-02-18 18:49:28 +0530

    get country data using api

--- src/app/page/register/register.component.ts
+++ src/app/page/register/register.component.ts
@@ -1,12 +1,37 @@
 import { Component } from '@angular/core';
+import { OnInit } from '@angular/core';
+import { HttpClient,HttpClientModule } from '@angular/common/http';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+
 
 @Component({
   selector: 'app-register',
   standalone: true,
-  imports: [],
+  imports: [HttpClientModule,CommonModule,FormsModule],
   templateUrl: './register.component.html',
   styleUrl: './register.component.css'
 })
-export class RegisterComponent {
+export class RegisterComponent implements OnInit{
+
+  private http;
+  public countryList:any;
+  constructor(private httpClient:HttpClient){
+      this.http = httpClient;
+  }
+
+  ngOnInit(): void {
+    this.loadCountry();
+  }
+
+  loadCountry(){
+      let api = "https://restcountries.com/v3.1/all";
+      this.http.get(api).subscribe(res=>{
+        this.countryList = res;
+      })
+  }
+
+  
+
 
 }
