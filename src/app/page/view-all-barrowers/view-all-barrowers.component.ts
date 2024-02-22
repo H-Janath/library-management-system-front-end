9c0fd73384df711580ce80136e112edadb78778b
janath <janathhma@gmail.com>
2024-02-22 21:47:41 +0530

    add delete and update modeal for barrower ui

--- src/app/page/view-all-barrowers/view-all-barrowers.component.ts
+++ src/app/page/view-all-barrowers/view-all-barrowers.component.ts
@@ -15,6 +15,7 @@ export class ViewAllBarrowersComponent  implements OnInit{
 
   private http;
   public barrowersList:any={};
+  public selectedBarrowe:any;
   
   constructor(private httpClient:HttpClient){
     this.http=httpClient;
@@ -24,14 +25,28 @@ export class ViewAllBarrowersComponent  implements OnInit{
     this.LoadBarrowers();
   }
 
+  selectBarrower(barrower: any){
+    console.log(barrower.bid);
+      this.selectedBarrowe = barrower;
+  }
+
   private LoadBarrowers(){
-    this.http.get('http://localhost:8080/barrowers/get').subscribe((data)=>{
+    this.http.get('http://localhost:8081/barrowers/get').subscribe((data)=>{
       console.log(data);
       this.barrowersList=data;
     })
   }
 
+  public deleteBarrower(){
+    let api = "http://localhost:8081/barrowers/delete"+this.selectedBarrowe.bid;
+      this.http.delete(api).subscribe((data)=>{
+          this.LoadBarrowers();
+      })
+  }
 
+  public saveBarrower(){
+
+  }
   
 
 }
